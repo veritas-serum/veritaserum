bc0046bf571c282e5c90fc1b5871010154a7323c
HediBY <hedi.benyounes@gmail.com>
2024-07-08 23:16:58 +0200

    Replaced exact match tests with Levenstein distance tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,6 +2,7 @@ import pytest
 from pathlib import Path 
 
 from src.article import Article, parse_url
+from src.utils import normalized_ed
 
 @pytest.mark.parametrize(
         "article_path",
@@ -14,6 +15,9 @@ from src.article import Article, parse_url
 def test_parse_url(article_path: str):
     expected_article = Article.from_json(Path(article_path).read_text())
     parsed_article = parse_url(expected_article.url)
+    dist_btw_titles = normalized_ed(expected_article.title, parsed_article.title)
+    dist_btw_contents = normalized_ed(expected_article.content, parsed_article.content)
+
+    assert dist_btw_titles <= 0.5
+    assert dist_btw_contents <= 0.5
     
-    assert expected_article.title == parsed_article.title
-    assert expected_article.content == parsed_article.content
