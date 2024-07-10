798073afd21124a129edf9ce3a0f8c3378301d55
HediBY <hedi.benyounes@gmail.com>
2024-07-10 22:27:50 +0200

    Removed unused properties of the Article class

--- src/article.py
+++ src/article.py
@@ -12,10 +12,6 @@ class Article:
     # title and content are instanciated by the url parser
     title: Optional[str] = None 
     content: Optional[str] = None
-    # suggested_title is instanciated by the LLM
-    suggested_title: Optional[str] = None 
-    # a metadata dict that should be further thought of
-    metadata: Optional[dict] = field(default_factory=dict)
 
 def parse_url(url: str) -> Article:
     title, content = article_parser.parse(url=url, output='markdown', timeout=5)
