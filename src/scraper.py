9e05ceb9642934799cbf1ffd426853cad7edc7db
HediBY <hedi.benyounes@gmail.com>
2024-07-10 15:34:53 +0200

    WIP scraper

--- src/scraper.py
+++ src/scraper.py
@@ -1,15 +1,45 @@
+import feedparser
 from src.article import Article 
+
+AUTHORIZED_TOPICS = [
+    "WORLD",
+    "NATION",
+    "BUSINESS",
+    "TECHNOLOGY",
+    "ENTERTAINMENT",
+    "SPORTS",
+    "SCIENCE ",
+]
 
 class GoogleNewsScraper:
     def __init__(self):
-        pass 
+        self.lang_suffix = "hl=fr&gl=FR&ceid=FR:fr"
+        self.query_suffix_template = "search?q={query}"
+        self.base_url = "https://news.google.com/rss"
+        self.topic_url_template = self.base_url + "/" + "headlines/section/topic/{topic}"
+
 
     def top_news(self) -> list[Article]:
-        pass 
+        url = self.base_url + self.lang_suffix
+        articles = self.retrieve_articles(url)
+        return articles
+        
 
     def query(self, query: str) -> list[Article]:
-        pass
+        query_suffix = self.query_suffix_template.format(query=query)
+        url = self.base_url + query_suffix + "&" + self.lang_suffix
+        articles = self.retrieve_articles(url)
+        return articles
+
 
     def top_news_from_topic(self, topic: str) -> list[Article]:
-        # https://news.google.com/news/rss/headlines/section/topic/{topic}?hl=fr&gl=FR&ceid=FR:fr
-        # topic in WORLD NATION BUSINESS TECHNOLOGY ENTERTAINMENT SPORTS SCIENCE HEALTH
+        # topic in WORLD NATION BUSINESS TECHNOLOGY ENTERTAINMENT SPORTS SCIENCE 
+        assert topic in AUTHORIZED_TOPICS
+        url = self.topic_url_template.format(topic = topic) + self.lang_suffix
+        articles = self.retrieve_articles(url)
+        return articles
+    
+    def retrieve_articles(self, url: str) -> list[Article]:
+        response = feedparser.parse(url)
+        import ipdb; ipdb.set_trace()
+
