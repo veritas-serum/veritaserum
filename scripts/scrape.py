702e6711caa612ffee698ec964a05d64deb6f3c7
HediBY <hedi.benyounes@gmail.com>
2024-07-10 19:04:39 +0200

    Added CLI and writing of parsed urls

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,8 +1,27 @@
+import argparse
+from pathlib import Path
 from src.scraper import GoogleNewsScraper, AUTHORIZED_TOPICS
 
+def cli_args():
+    parser = argparse.ArgumentParser(
+        "GoogleNews Scraper",
+        "This script parses GoogleNews using Google News RSS feed."
+    )
+    parser.add_argument(
+        "-f", 
+        "--output-file", 
+        help="Path where the scraped urls will be written.",
+    )
+    return parser.parse_args()
 
 if __name__ == "__main__":
+    args = cli_args()
     scraper = GoogleNewsScraper()
     urls = scraper.top_news()
     for topic in AUTHORIZED_TOPICS:
         urls += scraper.top_news_from_topic(topic)
+    urls = list(set(urls))
+    output_file = Path(args.output_file).open('a')
+
+    for url in urls:
+        output_file.write(url + "\n")
