04a9147af883258b845610a5e279d918e63347f0
Nicolas Lecrique <nicolaslecrique@hotmail.com>
2024-07-15 22:55:58 +0200

    rename and add comments

--- scripts/build_title_evaluation_dataset.py
+++ scripts/build_title_evaluation_dataset.py
@@ -1,12 +1,16 @@
 import json
 import pandas as pd
  
-with open('./data/babel-briefings-v1-fr.json') as f:
+# json File has been downloaded from
+# https://huggingface.co/datasets/felixludos/babel-briefings
+# into ./downloaded_data
+# TODO: Lazy-download this file directly inside the script
+with open('./downloaded_data/babel-briefings-v1-fr.json') as f:
     data = json.load(f)
  
-df = pd.json_normalize(data)
-df = df[['ID', 'url', 'title', 'description']]
+articles_df = pd.json_normalize(data)
+articles_df = articles_df[['ID', 'url', 'title', 'description']]
 
-df = df.sample(n=500, random_state=1)
+articles_df = articles_df.sample(n=500, random_state=1)
 
-df.to_csv("./data_generated/french_evaluation_sample.csv")
+articles_df.to_csv("./data_generated/french_evaluation_sample.csv")
